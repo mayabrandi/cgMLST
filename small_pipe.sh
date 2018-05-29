17d43d5b56303122a4c5de5cdaf2a4c6b6df3d7a
mayabrandi <maya.brandi@scilifelab.se>
2018-05-29 10:33:59 +0200

    Update small_pipe.sh

--- small_pipe.sh
+++ small_pipe.sh
@@ -4,7 +4,6 @@ i=`pwd|rev|cut -f 1 -d '/'|rev`
 ref=/home/maya.brandi/opt/cgMLST/E.coli/ref/GCF_000005845.2_ASM584v2_cds_from_genomic.fna
 
 bwa mem -R "@RG\tID:${i}\tSM:${i}" -t 4 -M -v 1  ${ref} ${i}_1.fastq.gz ${i}_2.fastq.gz > ${i}.sam
-echo 'hej'
 samtools view -Sb ${i}.sam >${i}.bam
 samtools sort ${i}.bam >${i}.sorted.bam
 samtools index ${i}.sorted.bam
